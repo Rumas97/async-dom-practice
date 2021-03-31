6f328716801ef88d7a4bb281853336e37dd3d43b
Rutul Maskar <rutulmaskar23@gmail.com>
2021-03-31 17:15:51 +0200

    async dom

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let timeDOM = document.querySelector('#timer')
 
 
 // STEP 3: Create an object from the timer class
+let timer = new Timer()
+console.log(Timer)
+console.log('This is the script file')
 
 function updateTime(){
+    timeDOM.innerHTML = timer.time
    // update the time DOM here
    // console.log('Here')
     
@@ -13,10 +17,12 @@ function updateTime(){
 
 
 startBtn.addEventListener('click', () => {
+    timer.start(updateTime)
     // Call the timer start method
 })
 
 
 stopBtn.addEventListener('click', () => {
+    timer.stop()
     // Call the timer stop method
 })
--- timer.js
+++ timer.js
@@ -1,13 +1,32 @@
 // add properties and methods for your timer here
 class Timer {
+
     //STEP 0: Add necessary properties for your timer
+    constructor(){
+        this.time= 0
+        this.intervalId=0
+        console.log('This the constructor in timer')
+    }
 
 
     // STEP1: Create a start method to start the timer
-
+    start(updateFunc){
+        console.log('Start called')
+        this.intervalId = setInterval(() => {
+            this.time++
+            updateFunc()
+     //     console.log(updateFunc)
+            console.log(this.time)
+        },1000)
+    }
 
 
     // STEP2: Create a stop method to stop the timer
-
+    stop(){
+        clearInterval(this.intervalId)
+        console.log('Stop called')
+    }
 
 }
+
+console.log('Timer file')
